7a6251bd677efd6eff4a2795497dcb88f267edd2
Kazung VII <arvid.anderson@outlook.com>
2022-05-18 20:49:14 +0200

    Created a .net minimal api with asp.net

---
+++ backend/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool Completed { get; set; }
+}
