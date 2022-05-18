e671ba975218c5797f993e4f5f4d889878c4363e
Kazung VII <arvid.anderson@outlook.com>
2022-05-18 13:00:09 +0200

    Created footer

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+      <div>
+        <p>Copyright © 2022 - Arvid Anderson</p>
+      </div>
+    </footer>
+  )
+}
+
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -3,11 +3,17 @@ import Hero from "../components/hero";
 export default function Home() {
   return (
     <div>
-      <Hero
-       title={"Fullstack"}
-       description={"this small fullstack web application is created with NextJS + ASP.NET Core."}
-       buttonTitle={"Try it out!"}
-      />
+
+      {/*Hero Section */}
+      <section className="HeroSection">
+        <Hero
+        title={"Fullstack"}
+        description={"this small fullstack web application is created with NextJS + ASP.NET Core."}
+        buttonTitle={"Try it out!"}
+        />
+      </section>
+
+      {/* Demo api section */}
     </div>
   )
 }
