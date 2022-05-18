0f155d5beb6b2f2b0d73e0445724135dc0d50797
Kazung VII <arvid.anderson@outlook.com>
2022-05-18 15:25:22 +0200

    Design & some onChange logic implemented

--- frontend/src/components/demoapi.jsx
+++ frontend/src/components/demoapi.jsx
@@ -1,16 +1,54 @@
 import { useState } from "react"
 
 export default function DemoApi() {
+  const methods = ["GET | Collection", "GET | Resource", "POST | Collection", "PUT | Resource", "PATCH | Resource", "DELETE | Resource"]
+  const [ selectedMethod, setSelectedMethod ] = useState(methods[0])
+  
+  const [ inputJson, setInputJson ] = useState("")
+
   const tabs = ["Raw", "Graphical"]
   const [ activeTab, setActiveTab ] = useState("Raw")
 
   return (
     <div className="min-h-screen bg-base-400 flex justify-center items-center p-10">
       <div className="flex flex-col w-full lg:flex-row">
-        <div className="grid h-32 flex-grow card bg-base-300 rounded-box place-items-center p-3">
-          Send Request Inteface
+
+        {/* Send Requuest */}
+        <div className="grid flex-grow card bg-base-300 rounded-box place-items-center p-3">
+          <div className="w-full">
+            {/* API URL Placeholder */}
+            <div className="flex w-full">
+              <input readOnly value={process.env.NEXT_PUBLIC_API_URL} type="text" placeholder={process.env.NEXT_PUBLIC_API_URL} class="input input-bordered input-accent w-full" />
+            </div>
+            {/* Select method*/}
+            <div className="flex w-full mt-3">
+              <select onChange={(e) => setSelectedMethod(e.target.value)} defaultValue={"Select request method.."} className="select select-secondary w-full">
+                <option disabled>Select request method..</option>
+                {methods.map((method) => (
+                  <option key={method} value={method}>{method}</option>
+                ))}
+              </select>
+            </div>
+
+            {/* Input Json / Text Area */}
+            <div className="mt-3 flex w-full h-full place-content-center place-items-center">
+              <textarea
+                onChange={(e) => setInputJson(e.target.value)} 
+                className="resize-none textarea textarea-secondary w-full h-full"
+                placeholder="">
+              </textarea>
+            </div>
+            {/* Send Request Button*/}
+            <div className="mt-3">
+              <button className="btn btn-success w-full flex md:w-36">Send Request</button>
+            </div>
+          </div>
         </div> 
+
+        {/* Divider */}
         <div className="divider lg:divider-horizontal"></div> 
+
+        {/* Results */}
         <div className="grid flex-grow card bg-base-300 rounded-box p-3">
           {/* Tabs */}
           <div className="tabs tabs-boxed mb-2">
@@ -18,21 +56,31 @@ export default function DemoApi() {
               <button key={tabs.indexOf(tab)} onClick={() => setActiveTab(tab)} className={activeTab === tab ? "tab tab-lg tab-active" : "tab tab-lg"}>{tab}</button> 
             ))}
           </div>
-          {/* Raw */}
-          {activeTab === tabs[0] && (
-            <div>
-              <textarea 
-                className="textarea textarea-primary w-full"
-                placeholder="{ id: 1, name: Arvid Anderson, age: 19 }">
-                
-              </textarea>
-            </div>
-          )}
-          {/* Graphical */}
-          <div>
-
+          <div className="flex h-72">
+            {/* Raw */}
+            {activeTab === tabs[0] && (
+              <div className="flex w-full h-full place-content-center place-items-center">
+                <textarea 
+                  className="resize-none textarea textarea-secondary w-full h-full"
+                  placeholder="{ id: 1, name: Arvid Anderson, age: 19 }">
+                  
+                </textarea>
+              </div>
+            )}
+            {/* Graphical */}
+            {activeTab === tabs[1] && (
+              <div className="flex w-full h-full">
+                <div class="card bg-secondary w-full h-full shadow-xl">
+                  <div class="card-body">
+                    <h2 class="card-title">Todo Title</h2>
+                    <p>Todo Description</p>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
+
       </div>      
     </div>
   )
